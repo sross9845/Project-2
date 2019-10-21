50cce463583ad67872414967cbb6f150fb759445
sross9845 <sean.ross.985@gmail.com>
2019-10-21 16:57:48 -0700

    got individual pages figured out

--- controllers/events.js
+++ controllers/events.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const passport = require('../config/ppconfig')
 const db = require('../models');
 const axios = require('axios').default;
+const tournamentsUrl = 'https://api.smash.gg/api/-/gg_api./public/tournaments/schedule;filter=%7B%22upcoming%22%3Atrue%2C%22videogameIds%22%3A7%7D;page=1;per_page=30;reset=false;schedule=true?returnMeta=true';
+const tournamentUrl = 'https://api.smash.gg/tournament/'
 
 
 
 router.get('/',function(req,res){
-    var url = 'https://api.smash.gg/api/-/gg_api./public/tournaments/schedule;filter=%7B%22upcoming%22%3Atrue%2C%22videogameIds%22%3A7%7D;page=1;per_page=30;reset=false;schedule=true?returnMeta=true';
-    axios.get(url).then(function(response){
+    axios.get(tournamentsUrl).then(function(response){
         res.render('events/global',{responses: response.data.items.entities.tournament})
     }).catch(function(err){
         console.log(err)
@@ -16,7 +17,9 @@ router.get('/',function(req,res){
 })
 
 router.get('/tournament/:slug',function(req,res){
-    res.send('whats up')
+    axios.get( tournamentUrl + req.params.slug).then(function(response){
+        res.send(response.data)
+    })
 })
 
 
