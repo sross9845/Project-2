3875a77e47a8e08e9bf915119b4472d65e34246e
sross9845 <sean.ross.985@gmail.com>
2019-10-24 14:29:29 -0700

    fixed error

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('../config/ppconfig')
+const passport = require('../config/ppConfig')
 const db = require('../models');
 
 
--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const isLoggedIn = require('../middleware/isLoggedIn')
 const db = require('../models');
 const methodOverride = require('method-override');
